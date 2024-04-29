dc816f90b6426bb2a6d13c2fec037ee053e12566
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-04-29 16:14:48 +0200

    added a bit of css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import './App.css';
+import React, { useState, useCallback } from 'react';
+import '../src/App.css';
 import Button from './components/Score/Score';
 import Card from './components/Card/Card';
 import Title from './components/Title/Title';
+
 function App() {
+  const [cards, setCards] = useState([/* Your initial card data here */]);
+  const [flippedCard, setFlippedCard] = useState(null);
+
+  const flipCard = useCallback((id) => {
+    setFlippedCard(id);
+  }, []);
+
+  const resetGame = useCallback(() => {
+    setCards([/* Reset your card data here */]);
+    setFlippedCard(null);
+  }, []);
+
   return (
     <div>
-      <Title/>
+      <Title />
+      {cards.map((card) => (
+        <Card key={card.id} card={card} flipCard={flipCard} />
+      ))}
+      <Button resetGame={resetGame} />
     </div>
   );
 }
-
 
 export default App;
