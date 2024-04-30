09cc729ff87ad7ec2cd7abde8d563eb8262149a9
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-04-30 11:24:16 +0200

    adding of back cards and background image

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import '../src/App.css';
-import Button from './components/Score/Score';
+import Button from './components/Button/Button';
 import Card from './components/Card/Card';
 import Title from './components/Title/Title';
 
 function App() {
-  const [cards, setCards] = useState([/* Your initial card data here */]);
-  const [flippedCard, setFlippedCard] = useState(null);
+  const [cards, setCards] = useState([
+    { id: 1, value: 'A', isFlipped: false, backImage: '../assets/cardreverse.png' },
+    { id: 2, value: 'A', isFlipped: false },
+    { id: 3, value: 'B', isFlipped: false },
+    { id: 4, value: 'B', isFlipped: false },
+    { id: 5, value: 'C', isFlipped: false },
+    { id: 6, value: 'C', isFlipped: false },
+    { id: 7, value: 'D', isFlipped: false },
+    { id: 8, value: 'D', isFlipped: false },
+  ]);
+  const [flippedCards, setFlippedCards] = useState([]);
 
   const flipCard = useCallback((id) => {
-    setFlippedCard(id);
+    setFlippedCards((prev) => [...prev, id]);
   }, []);
 
   const resetGame = useCallback(() => {
-    setCards([/* Reset your card data here */]);
-    setFlippedCard(null);
-  }, []);
+    setCards(cards.map(card => ({ ...card, isFlipped: false })));
+    setFlippedCards([]);
+  }, [cards]);
+
+  useEffect(() => {
+    if (flippedCards.length === 2) {
+      const card1 = cards.find(card => card.id === flippedCards[0]);
+      const card2 = cards.find(card => card.id === flippedCards[1]);
+
+      if (card1.value === card2.value) {
+        setCards(cards.map(card => {
+          if (card.id === card1.id || card.id === card2.id) {
+            return { ...card, isFlipped: true };
+          }
+          return card;
+        }));
+      }
+
+      setFlippedCards([]);
+    }
+  }, [flippedCards, cards]);
 
   return (
     <div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import backImage from '../../assets/cardreverse.png'; // Utilisez le chemin absolu
 
 const Card = ({ card, flipCard }) => {
+    const cardImage = `/assets/${card.value}.png`;
     return (
         <div onClick={() => flipCard(card.id)}>
-            {card.isFlipped ? card.value : ''}
+            {card.isFlipped ? <img src={cardImage} alt={card.value} /> : <img src={backImage} alt="dos de la carte" />}
         </div>
     );
 };
