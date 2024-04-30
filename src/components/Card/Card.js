8ed12325a5e5765b0973f0ac68b601238b968b97
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-04-30 14:31:09 +0200

    first card when flipped added

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,15 @@
+import React from 'react';
 import backImage from '../../assets/cardreverse.png'; // Utilisez le chemin absolu
+import anotherImage from '../../assets/card1.png'; // Importez l'image que vous voulez afficher
 
 const Card = ({ card, flipCard }) => {
-    const cardImage = `/assets/${card.value}.png`;
     return (
         <div onClick={() => flipCard(card.id)}>
             {card.isFlipped ? 
-                <img className="back-Image" src={cardImage} alt={card.value} /> : 
+                <img className="back-Image" src={anotherImage} alt="face de la carte" /> : 
                 <img className="back-Image" src={backImage} alt="dos de la carte" />}
         </div>
     );
 };
-
 
 export default Card;
