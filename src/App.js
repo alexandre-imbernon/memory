3ee8f0a3355d1dcdff28e1678f49789f6343e61b
alexandre-imbernon <alexandre.imbernon@laplateforme.io>
2024-04-30 16:13:00 +0200

    css heavily retouched

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import '../src/App.css';
+import './App.css';
 import Button from './components/Button/Button';
 import Card from './components/Card/Card';
 import Title from './components/Title/Title';
 import BackgroundMusic from './components/Music/Music';
 
+const shuffleCards = (cards) => {
+  // Mélanger les cartes de manière aléatoire
+  return cards.sort(() => Math.random() - 0.5);
+};
 
 function App() {
-  const [cards, setCards] = useState([
-    
-    { id: 1, value: 'A', isFlipped: false, backImage: '../assets/cardreverse.png' },
+  const initialCards = [
+    { id: 1, value: 'A', isFlipped: false },
     { id: 2, value: 'A', isFlipped: false },
     { id: 3, value: 'B', isFlipped: false },
     { id: 4, value: 'B', isFlipped: false },
@@ -17,39 +20,54 @@ function App() {
     { id: 6, value: 'C', isFlipped: false },
     { id: 7, value: 'D', isFlipped: false },
     { id: 8, value: 'D', isFlipped: false },
-  ]);
+    // Ajouter de nouvelles paires de cartes
+    //{ id: 9, value: 'E', isFlipped: false },
+    //{ id: 10, value: 'E', isFlipped: false },
+  ];
+
+  const [cards, setCards] = useState(shuffleCards([...initialCards]));
   const [flippedCards, setFlippedCards] = useState([]);
 
   const flipCard = useCallback((id) => {
-    setFlippedCards((prev) => [...prev, id]);
-  }, []);
+    if (flippedCards.length < 2) {
+      setFlippedCards((prev) => [...prev, id]);
+    }
+  }, [flippedCards]);
 
   const resetGame = useCallback(() => {
-    setCards(cards.map(card => ({ ...card, isFlipped: false })));
+    setCards(shuffleCards([...initialCards]));
     setFlippedCards([]);
-  }, [cards]);
+  }, [initialCards]);
 
   useEffect(() => {
     if (flippedCards.length === 2) {
-      const card1 = cards.find(card => card.id === flippedCards[0]);
-      const card2 = cards.find(card => card.id === flippedCards[1]);
+      const [firstId, secondId] = flippedCards;
+
+      const card1 = cards.find(card => card.id === firstId);
+      const card2 = cards.find(card => card.id === secondId);
 
       if (card1.value === card2.value) {
-        setCards(cards.map(card => {
-          if (card.id === card1.id || card.id === card2.id) {
-            return { ...card, isFlipped: true };
-          }
-          return card;
-        }));
+        // Si les cartes correspondent, elles restent retournées
+        setCards((prev) => {
+          return prev.map(card => {
+            if (card.id === firstId || card.id === secondId) {
+              return { ...card, isFlipped: true };
+            }
+            return card;
+          });
+        });
       }
 
-      setFlippedCards([]);
+      // Réinitialiser les cartes retournées après une pause pour permettre aux joueurs de voir les cartes
+      setTimeout(() => {
+        setFlippedCards([]);
+      }, 1000);
     }
   }, [flippedCards, cards]);
 
   return (
-    <div>
-      <BackgroundMusic /> {/* Ajoutez cette ligne pour utiliser le composant BackgroundMusic */}
+    <div className="App">
+      <BackgroundMusic />
       <Title />
       <div className="card-container">
         {cards.map((card) => (
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import backImage from '../../assets/cardreverse.png'; // Utilisez le chemin absolu
-import anotherImage from '../../assets/card1.png'; // Importez l'image que vous voulez afficher
+import backImage from '../../assets/cardreverse.png'; // Dos de la carte
+import cardAImage from '../../assets/card1.png'; // Face de la carte A
+import cardBImage from '../../assets/card2.png'; // Face de la carte B
+import cardCImage from '../../assets/card3.png'; // Face de la carte C
+import cardDImage from '../../assets/card4.png'; // Face de la carte D
+
+const getCardImage = (value) => {
+  switch (value) {
+    case 'A':
+      return cardAImage;
+    case 'B':
+      return cardBImage;
+    case 'C':
+      return cardCImage;
+    case 'D':
+      return cardDImage;
+    default:
+      return null;
+  }
+};
 
 const Card = ({ card, flipCard }) => {
-    return (
-        <div onClick={() => flipCard(card.id)}>
-            {card.isFlipped ? 
-                <img className="back-Image" src={anotherImage} alt="face de la carte" /> : 
-                <img className="back-Image" src={backImage} alt="dos de la carte" />}
-        </div>
-    );
+  const frontImage = getCardImage(card.value);
+
+  return (
+    <div onClick={() => flipCard(card.id)}>
+      {card.isFlipped ? (
+        <img className="card-face" src={frontImage} alt={`face de la carte ${card.value}`} />
+      ) : (
+        <img className="card-back" src={backImage} alt="dos de la carte" />
+      )}
+    </div>
+  );
 };
 
 export default Card;
